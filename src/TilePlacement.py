3ea45483c3802c3825584dabceba973fc3cf1295
Mathis <146923786+Pathis13@users.noreply.github.com>
2025-10-10 13:57:31 +0200

    refactor TilePlacement.py

--- src/TilePlacement.py
+++ src/TilePlacement.py
@@ -22,84 +22,74 @@ def create_tile(grid):
     
     return img
 
-empty_tile = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
-plus_tile = [
-    [0, 1, 0],
-    [1, 1, 1],
-    [0, 1, 0]
-]
 
-TriangleUp_tile = [
+tiles = []
+tiles.append([ #empty
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+])
+tiles.append([ #triangle up
     [0, 1, 0],
     [1, 1, 1],
     [0, 0, 0]
-]
-TriangleDown_tile = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0]
-]
-TriangleLeft_tile = [
+])
+tiles.append([ #triangle right
     [0, 1, 0],
     [0, 1, 1],
     [0, 1, 0]
-]
-TriangleRight_tile = [
+])
+tiles.append([ #triangle down
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+])
+tiles.append([ #triangle left
     [0, 1, 0],
     [1, 1, 0],
     [0, 1, 0]
-]
-CornerUpLeft_tile = [
+])
+tiles.append([ #corner up right
     [0, 1, 0],
     [0, 1, 1],
     [0, 0, 0]
-]
-CornerDownLeft_tile = [
+])
+tiles.append([ #corner right down
     [0, 0, 0],
     [0, 1, 1],
     [0, 1, 0]
-]
-CornerUpRight_tile = [
-    [0, 1, 0],
-    [1, 1, 0],
-    [0, 0, 0]
-]
-CornerDownRight_tile = [
+])
+tiles.append([ #corner down left
     [0, 0, 0],
     [1, 1, 0],
     [0, 1, 0]
-]
-VerticalLine_tile = [
+])
+tiles.append([ #corner left up
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0]
+])
+tiles.append([ #plus
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+])
+tiles.append([ #vertical
     [0, 1, 0],
     [0, 1, 0],
     [0, 1, 0]
-]
-HorizontalLine_tile = [
+])
+tiles.append([ #horizontal
     [0, 0, 0],
     [1, 1, 1],
     [0, 0, 0]
-]
+])
 
-empty_image = create_tile(empty_tile)
-plus_image = create_tile(plus_tile)
-TriangleUp_image = create_tile(TriangleUp_tile)
-TriangleDown_image = create_tile(TriangleDown_tile)
-TriangleLeft_image = create_tile(TriangleLeft_tile)
-TriangleRight_image = create_tile(TriangleRight_tile)
-CornerUpLeft_image = create_tile(CornerUpLeft_tile)
-CornerDownLeft_image = create_tile(CornerDownLeft_tile)
-CornerUpRight_image = create_tile(CornerUpRight_tile)
-CornerDownRight_image = create_tile(CornerDownRight_tile)
-VerticalLine_image = create_tile(VerticalLine_tile)
-HorizontalLine_image = create_tile(HorizontalLine_tile)
 
-tiles = [empty_image, plus_image, TriangleUp_image, TriangleDown_image, TriangleLeft_image, TriangleRight_image, CornerUpLeft_image,
-          CornerDownLeft_image, CornerUpRight_image, CornerDownRight_image, VerticalLine_image, HorizontalLine_image]
+image = []
 
+for i in range(len(tiles)):
+    image.append(create_tile(tiles[i]))
 
 combined_width = tile_width * size
 combined_height = tile_height * size
@@ -123,7 +113,7 @@ with open("maze.txt", "r") as fichier:
 
 for y in range(size):
     for x in range(size):
-        combined_image.paste(tiles[int(labyrinthe[x][y])], (tile_width*x, tile_height*y))
+        combined_image.paste(image[int(labyrinthe[y][x])], (tile_width*x, tile_height*y))
 
 combined_image.show(title="maze")
 combined_image.save("labyrinthe.png")
