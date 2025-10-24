7419be956649d08698b6aac9aca49d1463108020
Mathis <mathis.andre@etu.univ-amu.fr>
2025-10-24 11:59:37 +0200

    ajout des parametres dans l'utilisation de l'api

--- src/recursive_backtracking/show_image.py
+++ src/recursive_backtracking/show_image.py
@@ -9,11 +9,7 @@ NB_CYCLES = 10
 NUM_WRAP_TUNNELS = 2
 NUM_CENTER_TUNNELS = 5
 
-# maze = Maze(WIDTH, HEIGHT, NB_CYCLES, NUM_WRAP_TUNNELS, NUM_CENTER_TUNNELS)
-# maze.generate_maze()
-
-url = "https://pacmaz-s1-o.onrender.com/generate"
-# url = "https://pacmaz-s1-o.onrender.com/generate?width={WIDTH}&height={HEIGHT}&height={NB_CYCLES}"
+url = f"https://pacmaz-s1-o.onrender.com/generate?width={WIDTH}&height={HEIGHT}&nb_cycle={NB_CYCLES}&num_tunnels_wrap={NUM_WRAP_TUNNELS}&num_tunnels_centre={NUM_CENTER_TUNNELS}"
 
 
 response = urlopen(url)
